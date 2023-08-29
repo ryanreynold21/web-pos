1e11038f5c740eed79318e09c5f67a77039cb536
Rikir-M <hsuyatimyo11@gmail.com>
2023-08-29 12:56:26 +0630

    sale-90%-done

--- src/pages/Cashier.jsx
+++ src/pages/Cashier.jsx
@@ -1,112 +1,109 @@
 import React, { useState } from "react";
 import { FiDelete } from "react-icons/fi";
-import Navbar from "../layout/Navbar";
 import { useGetProductQuery } from "../services/authApi";
+import { useNavigate } from 'react-router-dom';
 
 const Cashier = () => {
-  const token = localStorage.getItem("token") 
-  const {data} = useGetProductQuery(token)
-  console.log(data)
-  const [products, setProducts] = useState([
-    {
-      id: 1,
-      name: "Xbox Series X",
-      quantity: "0",
-      price: 500,
-      img: "https://news.xbox.com/en-us/wp-content/uploads/sites/2/2020/03/XboxSeriesXController_HERO.jpg",
-    },
-    {
-      id: 2,
-      name: "Xbox Series X2",
-      quantity: "0",
-      price: 500,
-      img: "https://news.xbox.com/en-us/wp-content/uploads/sites/2/2020/03/XboxSeriesXController_HERO.jpg",
-    },
-    {
-      id: 3,
-      name: "Xbox Series X3",
-      quantity: "0",
-      price: 500,
-      img: "https://news.xbox.com/en-us/wp-content/uploads/sites/2/2020/03/XboxSeriesXController_HERO.jpg",
-    },
-    {
-      id: 4,
-      name: "Xbox Series X4",
-      quantity: "0",
-      price: 500,
-      img: "https://news.xbox.com/en-us/wp-content/uploads/sites/2/2020/03/XboxSeriesXController_HERO.jpg",
-    },
-    {
-      id: 5,
-      name: "Xbox Series X5",
-      quantity: "0",
-      price: 500,
-      img: "https://news.xbox.com/en-us/wp-content/uploads/sites/2/2020/03/XboxSeriesXController_HERO.jpg",
-    },
-  ]);
-  const [selectedProduct, setSelectedProduct] = useState(1);
+  const navigate = useNavigate();
+  const token = localStorage.getItem("token");
+  const { data } = useGetProductQuery(token);
+  const [selectedProducts, setSelectedProducts] = useState([]);
+  const [selectedQuantities, setSelectedQuantities] = useState({});
+  const [currentSelectedProductId, setCurrentSelectedProductId] = useState(null);
 
-  const handleNumberClick = (num) => {
-    const updatedProducts = products.map((product) => {
-      if (product.id === selectedProduct) {
-        const newQuantity =
-          product.quantity === "0" ? num : product.quantity + num;
-        return { ...product, quantity: newQuantity };
+
+  const handleProductClick = (productId) => {
+    setSelectedProducts((prevSelected) => {
+      if (prevSelected.includes(productId)) {
+        const updatedQuantities = { ...selectedQuantities };
+        updatedQuantities[productId] = (updatedQuantities[productId] || 0) + 1;
+        setSelectedQuantities(updatedQuantities);
+        return prevSelected;
+      } else {
+        const updatedSelected = [...prevSelected, productId];
+        const updatedQuantities = { ...selectedQuantities };
+        updatedQuantities[productId] = 1;
+        setSelectedQuantities(updatedQuantities);
+        return updatedSelected;
       }
-      return product;
     });
-    setProducts(updatedProducts);
   };
 
-  const handleClearClick = (num) => {
-    const updatedProducts = products.map((product) => {
-      if (product.id === selectedProduct) {
-        if (product.quantity === "0") {
-          return { ...product, selected: false };
-        }
-        const delQuantity =
-          product.quantity.length === 1 ? "0" : product.quantity.slice(0, -1);
-        return { ...product, quantity: delQuantity };
+  const handleNumberClick = (number) => {
+    if (currentSelectedProductId !== null) {
+      setSelectedQuantities((prevQuantities) => {
+        const currentQuantity = prevQuantities[currentSelectedProductId] || 0;
+        const newQuantity = parseInt(`${currentQuantity}${number}`);
+        const updatedQuantities = {
+          ...prevQuantities,
+          [currentSelectedProductId]: newQuantity,
+        };
+        return updatedQuantities;
+      });
+    }
+  };
+
+  const handleClearClick = () => {
+    setSelectedQuantities((prevQuantities) => {
+      const productId = currentSelectedProductId;
+      const currentQuantity = prevQuantities[productId] || 0;
+      const newQuantity = Math.floor(currentQuantity / 10); 
+      const updatedQuantities = {
+        ...prevQuantities,
+        [productId]: newQuantity >= 1 ? newQuantity : 0,
+      };
+
+      if (newQuantity <= 0) {
+        setSelectedProducts((prevSelected) =>
+          prevSelected.filter((id) => id !== productId)
+        );
       }
-      return product;
+
+      return updatedQuantities;
     });
-    setProducts(updatedProducts);
   };
 
   const calculateTotal = () => {
     let total = 0;
-    products.forEach((product) => {
-      if (product.selected) {
-        total += product.price * product.quantity;
+
+    selectedProducts.forEach((productId) => {
+      const selectedProduct = data?.data?.find(
+        (product) => product.id === productId
+      );
+
+      if (selectedProduct) {
+        const quantity = selectedQuantities[productId] || 0;
+        total += selectedProduct.price * quantity;
       }
     });
+
     return total;
   };
 
-  const handleProductSelect = (productId) => {
-    setSelectedProduct(productId);
-    const updatedProducts = products.map((product) => ({
-      ...product,
-      selected: product.id === productId || product.selected,
-      quantity:
-        product.id === productId
-          ? String(parseInt(product.quantity) + 1)
-          : product.quantity,
-    }));
-    setProducts(updatedProducts);
-  };
+
   const handleProductInCartSelect = (productId) => {
-    setSelectedProduct(productId);
-    const updatedProducts = products.map((product) => ({
-      ...product,
-      selected: product.id === productId || product.selected,
-    }));
-    setProducts(updatedProducts);
+    setCurrentSelectedProductId(productId);
+    setSelectedProducts((prevSelected) => {
+      if (!prevSelected.includes(productId)) {
+        return [...prevSelected, productId];
+      }
+      return prevSelected;
+    });
+  };
+
+  const handlePaymentClick = () => {
+    const total = calculateTotal(); 
+    const checkoutData = {
+      selectedProducts,
+      selectedQuantities,
+      total,
+      data,
+    };
+    navigate('/checkout', { state: { checkoutData } });
   };
 
   return (
     <div className="bg-[#272727]">
-      {/* <Navbar/> */}
       <div className="flex gap-5 ">
         <div className="w-3/4  min-h-screen">
           <div className="flex justify-between mx-5 my-5">
@@ -137,20 +134,19 @@ const Cashier = () => {
                 id="default-search"
                 class="block py-2 pl-10 pr-3 text-sm text-[#B19777] border border-[#B19777] rounded-lg bg-[#272727]   placeholder-[#B19777]  focus:outline-none"
                 placeholder="Search ..."
-                // value={searchTerm} onChange={(e)=>dispatch(setSearchTerm(e.target.value))}
               />
             </div>
           </div>
           <div className="grid grid-cols-4 gap-3">
-            {products.map((product) => (
+            {data?.data?.map((product) => (
               <div
                 key={product.id}
-                className={`product cursor-pointer h-[250px] max-w-sm bg-[#323232] border border-[#B19777] rounded-lg shadow dark:bg-[#323232] dark:border-[#B19777]" ${
-                  product.selected ? "active" : ""
+                className={`product cursor-pointer h-[280px] w-[250px] bg-[#323232] border border-[#B19777] rounded-lg shadow dark:bg-[#323232] dark:border-[#B19777]" ${
+                  selectedProducts.includes(product.id) ? "active" : ""
                 }`}
-                onClick={() => handleProductSelect(product.id)}
+                onClick={() => handleProductClick(product.id)}
               >
-                <img src={product.img} className=" rounded-t-lg" alt="" />
+                <img src={product.photo} className=" rounded-t-lg w-full h-2/3" alt="" />
                 <div className="p-5">
                   <h5 className="mb-2 text-2xl font-bold tracking-tight text-[#B19777]">
                     {product.name}
@@ -169,114 +165,125 @@ const Cashier = () => {
               Cart
             </h1>
             <div className="overflow-auto w-full h-2/3">
-              {products.map(
-                (product) =>
-                  product.selected && (
-                    <div
-                      key={product.id}
-                      className={`display flex py-1 px-4 justify-between items-center transition-all border-b border-b-[#B19777] ${
-                        product.id === selectedProduct ? "prodInCartActive" : ""
-                      }`}
-                      onClick={() => handleProductInCartSelect(product.id)}
-                    >
-                      <div className="">
-                        <h5 className="mb-2 text-2xl tracking-tight text-[#B19777]">
-                          {product.name}
-                        </h5>
-                        <p className="mb-3 font-normal text-gray-400">
-                          {product.quantity}
-                        </p>
-                      </div>
+              {selectedProducts.map((productId) => {
+                const selectedProduct = data?.data?.find(
+                  (product) => product.id === productId
+                );
+
+                if (!selectedProduct) {
+                  return null; 
+                }
+                const quantity = selectedQuantities[productId] || 0; 
+                const totalPrice = selectedProduct.price * quantity;
+
+                return (
+                  <div
+                    key={productId}
+                    className={`display cursor-pointer flex py-1 px-4 justify-between items-center transition-all border-b border-b-[#B19777] ${
+                      selectedProduct.id === currentSelectedProductId
+                        ? "prodInCartActive"
+                        : ""
+                    }`}
+                    onClick={()=>handleProductInCartSelect(productId)}
+                  >
+                    <div className="">
+                      <h5 className="mb-2 text-2xl tracking-tight text-[#B19777]">
+                        {selectedProduct.name}
+                      </h5>
                       <p className="mb-3 font-normal text-gray-400">
-                        {product.price * product.quantity}
+                        {quantity}
                       </p>
                     </div>
-                  )
-              )}
+                    <p className="mb-3 font-normal text-gray-400">
+                      {totalPrice}
+                    </p>
+                  </div>
+                );
+              })}
             </div>
             <p className=" text-2xl ml-52">Total : {calculateTotal()}</p>
           </div>
           <div className="h-1/2 bg-[#323232]">
             <div className="buttons text-gray-400">
-              <div className="grid grid-cols-4 h-20">
+              <div className="grid grid-cols-3 h-20">
                 <button
-                  onClick={() => handleNumberClick("7")}
+                  onClick={() => handleNumberClick( "7")}
                   className="col-span-1 number-btn"
                 >
                   7
                 </button>
                 <button
-                  onClick={() => handleNumberClick("8")}
+                  onClick={() => handleNumberClick( "8")}
                   className="col-span-1 number-btn"
                 >
                   8
                 </button>
                 <button
-                  onClick={() => handleNumberClick("9")}
+                  onClick={() => handleNumberClick( "9")}
                   className="col-span-1 number-btn"
                 >
                   9
                 </button>
-                <button
+                {/* <button
                   // onClick={() => handleNumberClick("9")}
                   className="col-span-1 number-btn"
                 >
                   Qty
-                </button>
+                </button> */}
               </div>
-              <div className="grid grid-cols-4 h-20">
+              <div className="grid grid-cols-3 h-20">
                 <button
-                  onClick={() => handleNumberClick("4")}
+                  onClick={() => handleNumberClick( "4")}
                   className="col-span-1 number-btn"
                 >
                   4
                 </button>
                 <button
-                  onClick={() => handleNumberClick("5")}
+                  onClick={() => handleNumberClick( "5")}
                   className="col-span-1 number-btn"
                 >
                   5
                 </button>
                 <button
-                  onClick={() => handleNumberClick("6")}
+                  onClick={() => handleNumberClick( "6")}
                   className="col-span-1 number-btn"
                 >
                   6
                 </button>
-                <button
+                {/* <button
                   // onClick={() => handleNumberClick("9")}
                   className="col-span-1 number-btn"
                 >
                   %Disc
-                </button>
+                </button> */}
               </div>
-              <div className="grid grid-cols-4 h-20">
+              <div className="grid grid-cols-3 h-20">
                 <button
-                  onClick={() => handleNumberClick("1")}
+                 onClick={() => handleNumberClick( "1")}
                   className="col-span-1 number-btn"
                 >
                   1
                 </button>
                 <button
-                  onClick={() => handleNumberClick("2")}
+                  onClick={() => handleNumberClick( "2")}
                   className="col-span-1 number-btn"
                 >
                   2
                 </button>
                 <button
-                  onClick={() => handleNumberClick("3")}
+                  onClick={() => handleNumberClick( "3")}
                   className="col-span-1 number-btn"
                 >
                   3
                 </button>
-                <button
+                {/* <button
                   // onClick={() => handleNumberClick("9")}
                   className="col-span-1 number-btn"
                 >
                   Price
-                </button>
+                </button> */}
               </div>
-              <div className="grid grid-cols-4 h-20">
+              <div className="grid grid-cols-3 h-20">
                 <button
                   // onClick={() => handleNumberClick("0")}
                   className="col-span-1 number-btn"
@@ -284,17 +291,17 @@ const Cashier = () => {
                   +/-
                 </button>
                 <button
-                  onClick={() => handleNumberClick("0")}
+                  onClick={() => handleNumberClick( "0")}
                   className="col-span-1 number-btn"
                 >
                   0
                 </button>
-                <button
-                  // onClick={() => handleNumberClick("9")}
+                {/* <button
+                  // onClick={() => handleNumberClick(".")}
                   className="col-span-1 number-btn"
                 >
                   .
-                </button>
+                </button> */}
                 <button
                   onClick={handleClearClick}
                   className="flex justify-center items-center number-btn"
@@ -305,7 +312,7 @@ const Cashier = () => {
             </div>
             <div className="grid grid-cols-1 bg-[#B19777]  text-white">
               <button
-                // onClick={() => handleNumberClick("9")}
+                onClick={ handlePaymentClick}
                 className=" number-btn h-[49px]"
               >
                 Payment
